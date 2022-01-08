0ba60f6df65fc65f823999452aaeb1cd71b1cd37
Matthew Blott <matthew.blott@gmail.com>
2022-01-08 13:17:15 +0000

    initial commit

---
+++ myapp/app/App.svelte
@@ -0,0 +1,11 @@
+<page>
+  <actionBar title="Home" />
+  <gridLayout>
+    <label class="info">
+      <formattedString>
+        <span class="fas" text="&#xf135;" />
+        <span text="Blank Svelte Native App" />
+      </formattedString>
+    </label>
+  </gridLayout>
+</page>
